4cf9661556ca10441827710a6b66628de884567c
221-alpha <mamadoualpha.diallo@edu.ece.fr>
2025-05-27 19:03:59 +0200

    Add files via upload

---
+++ Evenements.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Evenement en Boutique</title>
+    <link rel="stylesheet" href="evenement.css">
+</head>
+<body>
+<header>
+    <h1>Les Evénements A Venir </h1>
+    <nav>
+        <ul>
+            <li><a href="Accueil.php">Accueil</a></li>
+            <li><a href="Catalogue.php">Catalogue</a></li>
+            <li><a href="Evenements.php">Evenement</a></li>
+        </ul>
+    </nav>
+</header>
+<section class="container">
+    <div class="bloc1">
+        <img src="real9.jpg" alt="Echec" width="50%" height="200px">
+    </div>
+    <div class="bloc2">
+        <p><strong>Tournoi Strategie: Le Roi des Jeux</strong>
+           <br> <b>Date:</b> 15 juin 2025
+            <br><strong>Durée:</strong> Journée complete
+            <br> <strong>Places disponibles:</strong> 12/20</br>
+            <strong>Description:</strong> Venez defier d'autres passionnés sur le meilleur jeu de strategie.
+            Plusieurs lots à gagner.</p>
+        <button type="button">S'INSCRIRE</button>
+    </div>
+</section>
+<section class="container">
+    <div class="bloc1">
+        <img src="img1.png" alt="Domino" width="50%" height="200px">
+    </div>
+    <div class="bloc2">
+        <p><strong>Tournoi Strategie: Le Roi du Domino</strong>
+            <br> <b>Date:</b> 29 juin 2025
+            <br><strong>Durée:</strong> Journée complete
+            <br> <strong>Places disponibles:</strong> 32/40</br>
+            <strong>Description:</strong> Le but est de se débarrasser de toutes ses tuiles avant ses adversaires.
+            Il exige observation, planification, et anticipation des coups adverses..</p>
+        <button type="button">S'INSCRIRE</button>
+    </div>
+</section>
+<section class="container">
+    <div class="bloc1">
+        <img src="img2.jpg" alt="Tic Tac Toe" width="50%" height="200px">
+    </div>
+    <div class="bloc2">
+        <p><strong>Tournoi Strategie: Tic Tac Toe </strong>
+            <br> <b>Date:</b> 15 juillet 2025
+            <br><strong>Durée:</strong> 10h à 14h
+            <br> <strong>Places disponibles:</strong> 12/20</br>
+            <strong>Description:</strong>Le but est simple : aligner trois symboles identiques, horizontalement,
+            verticalement ou en diagonale… avant son adversaire.</p>
+        <button type="button">S'INSCRIRE</button>
+    </div>
+</section>
+<section class="container">
+    <div class="bloc1">
+        <img src="img3.png" alt="MISTIKUBE" width="50%" height="200px">
+    </div>
+    <div class="bloc2">
+        <p><strong>Tournoi Strategie: MISTIKUBE</strong>
+            <br> <b>Date:</b> 29 Juillet 2025
+            <br><strong>Durée:</strong> 10H à 14H
+            <br> <strong>Places disponibles:</strong> 12/20</br>
+            <strong>Description:</strong> En plaçant des fragments de code dans la bonne configuration, vous activez les zones du Kube.
+            Chaque partie est un puzzle coopératif où la logique, la communication
+            et l’adaptation collective sont essentielles.</p>
+        <button type="button">S'INSCRIRE</button>
+    </div>
+</section>
+
+</body>
+</html>
